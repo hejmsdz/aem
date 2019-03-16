ccfab8edc9ceb4b18c62008495a952346046738a
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2019-03-16 19:44:04 +0100

    move unit tests

--- test/data_import_test.py
+++ test/data_import_test.py
@@ -7,7 +7,7 @@ from aem.lab1.data_import import load_points, distance_matrix
 
 class DataImportTest(unittest.TestCase):
     def test_load_points(self):
-        filename = os.path.join(os.path.dirname(__file__), '../../data/three.txt')
+        filename = os.path.join(os.path.dirname(__file__), '../data/three.txt')
         points = load_points(filename)
         expected_points = np.array([[0, 0], [4, 0], [0, 3]])
         np.testing.assert_equal(points, expected_points)
