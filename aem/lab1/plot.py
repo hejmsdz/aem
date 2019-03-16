dfb42bd28ee02e06e165c9f45cd27c377315457c
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2019-03-16 23:15:32 +0100

    use k-means for testing and plot groups

--- aem/lab1/plot.py
+++ aem/lab1/plot.py
@@ -1,7 +1,27 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pylab
 
-def plot_solution(points, clusters):
+from .mst import MST
+
+def plot_solution(points, distances, labels):
     x, y = points.T
-    plt.scatter(x, y)
+    colors = [f"C{i}" for i in labels]
+    plt.scatter(x, y, c=colors)
+
+    clusters = [set() for _ in range(max(labels) + 1)]
+    for (i, label) in enumerate(labels):
+        clusters[label].add(i)
+
+    lines = []
+    for i, cluster in enumerate(clusters):
+        mst = MST(distances, cluster)
+        mst.find()
+        for (a, b) in mst.tree:
+            x1, y1 = points[a]
+            x2, y2 = points[b]
+            lines.extend([(x1, x2), (y1, y2), f"C{i}"])
+
+    plt.plot(*lines)
+
     pylab.show()
