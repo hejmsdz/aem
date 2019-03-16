7f0a67b9149b20ad4206b36bc3799f2b03b1bb58
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2019-03-16 23:22:59 +0100

    cleanup

--- aem/lab1/cost.py
+++ aem/lab1/cost.py
@@ -3,11 +3,9 @@ import numpy as np
 from .mst import MST
 
 def clustering_cost(distances, clusters):
-    print('testing')
-    cost = 0
+    cost = 0.0
     for cluster in clusters:
         mst = MST(distances, cluster)
         mst.find()
-        print(mst.tree)
         cost += mst.cost
     return cost
